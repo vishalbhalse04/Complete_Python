938fa3ee054fcfd64ff427e633ad4135b2240805
vishalbhalse04 <vishalbhalse.04@gmail.com>
2025-06-18 19:13:48 +0530

    Create Variables_file.py

---
+++ First_Day/Variables_file.py
@@ -0,0 +1,9 @@
+Name = "vishal bhalse"   # This is a string type variable
+Age = 20                 # This is a integer type variable
+Hight = 5.5              # This is a float type variable
+is_student = True        # This is a boolean type variable
+
+print("Name : ", Name)  # Text in double quotes print as it is and after comma variable_name 
+print("age : ", Age)    
+print("Hight :", Hight)
+print("is_student :", is_student)
