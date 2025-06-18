0f0a6cdcc448668f1f4d59bd1b7499ebfef1dda6
vishalbhalse04 <vishalbhalse.04@gmail.com>
2025-06-18 19:29:35 +0530

    Update Variables_file.py

--- First_Day/Variables_file.py
+++ First_Day/Variables_file.py
@@ -7,3 +7,7 @@ print("Name : ", Name)  # Text in double quotes print as it is and after comma v
 print("age : ", Age)    
 print("Hight :", Hight)
 print("is_student :", is_student)
+
+
+Name = input("Enter your name : ")   # Input function use to take input from user that's wy we use input()
+print("Your name is : ", Name)
