c124a71f795ecf5b73680a7d9c8f2583c011d6b4
vishalbhalse04 <vishalbhalse.04@gmail.com>
2025-06-18 19:31:45 +0530

    Create Arithmatics_Opraters.py

---
+++ First_Day/Arithmatics_Opraters.py
@@ -0,0 +1,9 @@
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
